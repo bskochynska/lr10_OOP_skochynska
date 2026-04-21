4a50a9590719a4a53c9a2f70676a5caa77f41e5a
Bohdana Skochynska <bohdanaskochynska@gmail.com>
2026-04-21 22:54:14 +0300

    Feat: implementation of Node removal in a BST

---
+++ Delete_Node_in_a_BST/code.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        if root is None:
+            return None
+        if key < root.val:
+            root.left = self.deleteNode(root.left, key)
+        elif key > root.val:
+            root.right = self.deleteNode(root.right, key)
+        else:
+            if root.left is None:
+                return root.right
+            if root.right is None:
+                return root.left
+            current = root.right
+            while current.left is not None:
+                current = current.left
+            root.val = current.val
+            root.right = self.deleteNode(root.right,current.val)
+        return root
